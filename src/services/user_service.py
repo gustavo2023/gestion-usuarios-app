ef12a171f136cec34b1c43865155d98115d002bf
Gustavo Gutierrez <gustavo2023@gmail.com>
2025-03-31 03:13:08 -0400

    feat: add logging for user creation and retrieval processes

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -3,6 +3,7 @@ from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 from models.user import User
 from passlib.context import CryptContext
+from utils.logger import logger
 
 # Crea un contexto de cifrado para las contraseñas
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -15,17 +16,22 @@ def validate_email(email: str) -> bool:
 # Función para crear un nuevo usuario en la base de datos
 # Recibe una sesión de base de datos y los datos del usuario
 def create_user(db: Session, nombre: str, email: str, password: str):
+    # Loggea el intento de creación de usuario
+    logger.info(f"Intentando crear usuario: {nombre} con email: {email}")
     # Verifica si todos los campos son obligatorios
     # Si falta alguno, lanza una excepción
     if not all([nombre, email, password]):
+        logger.error("Campos faltantes en la creacion de usuario")
         raise ValueError("Todos los campos son obligatorios")
     
     # Verifica si el email tiene un formato válido
     # Si no es válido, lanza una excepción
     if not validate_email(email):
+        logger.error("Formato de email invalido")
         raise ValueError("Formato de email invalido")
     
     if db.query(User).filter(User.email == email).first():
+        logger.warning(f"El email ya está registrado: {email}")
         raise ValueError("El email ya está registrado")
     
     hased_password = pwd_context.hash(password)  # Cifra la contraseña
@@ -38,12 +44,15 @@ def create_user(db: Session, nombre: str, email: str, password: str):
         db.add(user)
         db.commit()
         db.refresh(user)
+        logger.info(f"Usuario creado exitosamente: {nombre}")
         return user
     except IntegrityError:
         db.rollback()
+        logger.error(f"Error al crear usuario: {nombre}, email: {email}")
         raise ValueError("El email ya esta registrado")
 
 # Función para obtener todos los usuarios de la base de datos
 # Recibe una sesión de base de datos y devuelve una lista de usuarios
 def get_all_users(db: Session):
+    logger.info("Obteniendo todos los usuarios")
     return db.query(User).order_by(User.id).all()
