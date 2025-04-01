007daf62baf6d99cd5f5f00ad7a5d643f1883e11
Gustavo Gutierrez <gustavo2023@gmail.com>
2025-03-31 21:10:37 -0400

    feat: adjust logging level for SQLAlchemy engine to reduce verbosity

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,8 +1,10 @@
 import logging
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.INFO,  
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 )
+
+logging.getLogger("sqlalchemy.engine").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
